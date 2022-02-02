80ced034b68f978981abe42159054aff4b5984fc
KassiaJRauen <kassia.grj@gmail.com>
2022-02-02 16:38:48 -0300

    add  /login and /sign-up

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {useState} from 'react';
 import { Container, StyledLink } from './styles';
 import { useNavigate } from 'react-router-dom';
@@ -9,27 +10,55 @@ function Login(){
   
     const navigate = useNavigate();
 
+    function handleLogin(e) {
+        setLoading(true);
+        e.preventDefault();
+
+        const promise = axios.post('http://localhost:5000/login', {email: email, password: password});
+        
+        setTimeout(() => {
+            promise.then(() => {
+            navigate('/home');
+        });
+        }, 1000);
+        promise.catch((error) => {
+            setLoading(false);
+            alert(error.response.data.message);
+        });
+
+    }
+
     return(
         <Container> 
                 <img src='../../assets/MyWallet.png' alt='' />
-                <input
-                type='email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                placeholder='E-mail'
-                />         
-                <input
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                placeholder='Senha'
-                />    
-                <button type='submit'>Entrar</button>
+                <form onSubmit={handleLogin}>
+                    <input
+                    type='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder='E-mail'
+                    />         
+                    <input
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder='Senha'
+                    />    
+                    <button type='submit'>{loading ? <Loading/> :'Entrar'}</button>
+                </form>
                 <StyledLink to='/sign-up'>
                     <span>Primeira vez? Cadastre-se!</span>
                 </StyledLink>
                 </Container>
     )
+
+    function Loading() {
+        return (
+          <span color='#FFFFFF' height={50} width={50}>
+            Carregando...
+          </span>
+        );
+      }
 }
 
 export default Login;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Container, StyledLink } from './styles';
 import { useNavigate } from 'react-router-dom';
@@ -7,44 +8,72 @@ function SingUp(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [loading, setLoading] = useState('false');
+
+    const navigate = useNavigate();
+
+    function handleSignup(e){
+    setLoading(true);
+    e.preventDefault();
+    
+    const promise = axios.post('http://localhost:5000/sign-up', {name: name, email: email, password: password, confirmPassword: confirmPassword});
+    setTimeout(() => {
+        promise.then(() => {
+        navigate('/');
+    });
+    }, 1000);
+    promise.catch((error) => {
+        setLoading(false);
+        alert(error.response.data.message);
+    });
+    }
 
     return(
         <Container>
             <img src='../../assets/MyWallet.png' alt='' />
-            <input
-            type='name'
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            placeholder='Nome'
-            required
-            />
-            <input
-            type='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder='E-mail'
-            required
-            />
-            <input
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder='Senha'
-            required
-            />
-            <input
-            type='confirmPassword'
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-            placeholder='Confirme a senha'
-            required
-            />
-            <button type='submit'>Cadastrar</button>
+            <form onSubmit={handleSignup}>
+                <input
+                type='name'
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                placeholder='Nome'
+                required
+                />
+                <input
+                type='email'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder='E-mail'
+                required
+                />
+                <input
+                type='password'
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder='Senha'
+                required
+                />
+                <input
+                type='password'
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+                placeholder='Confirme a senha'
+                required
+                />
+                <button type='submit'>{!loading ? <Loading/> : 'Cadastrar'}</button>
+            </form>
             <StyledLink to='/'>
                 <span>Já tem uma conta? Entre agora!</span>
             </StyledLink>
         </Container>
     )
-}
 
+    function Loading() {
+        return (
+          <span color='#FFFFFF' height={50} width={50}>
+            Carregando...
+          </span>
+        );
+      }
+}
 export default SingUp;
